668a17816f333148a0f5b447c0beeb313c4d2ba9
yan <yyan_0122@163.com>
2022-08-01 21:18:27 +0800

    4-5 制作the-header、the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
-    <router-view />
-    <a-layout-footer style="text-align: center">
-      Yan's Wiki ©2022 Created by Yan
-    </a-layout-footer>
+    <the-header></the-header>
+    <router-view/>
+    <the-footer></the-footer>
   </a-layout>
 </template>
+
+<script lang="ts">
+  import {defineComponent} from 'vue';
+  import TheHeader from '@/components/the-header.vue'
+  import TheFooter from "@/components/the-footer.vue"
+
+  export default defineComponent({
+    name: 'app',
+    components:{
+      TheFooter,
+      TheHeader,
+    }
+  });
+</script>
 
 <style>
 #components-layout-demo-top-side-2 .logo {
